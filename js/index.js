ab74d1ce2c1a1d15ada60959498875689b31c4ea
zjh <1429823270@qq.com>
2017-12-03 10:17:51 +0800

    新增了左右bar

--- js/index.js
+++ js/index.js
@@ -1,44 +1,9 @@
 $(function () {
-
-
-
-    var status = 0;
-    //头部导航特效
-    //鼠标放在第二个导航图标显示
-    $('.nav li').eq(1).hover(function () {
-        $('.class-name').show()
-    },function () {
-        $('.class-name').hide()
-    });
-
-//    搜索框放大
-    $('.search').focus(function () {
-        $(this).stop().animate({'width':'120px'},500).blur(function () {
-            $(this).stop().animate({'width':'70px'},500)
-        })
-    })
-//    二维码的显示和隐藏
-    $('#last .last_content .right .bottom>span').mouseenter(function(){
-        if(status===0){
-            status = 1;
-        }else{
-            return;
-        }
-        $(this).children('div').fadeIn(200);
-    }).mouseleave(function(){
-        $(this).children('div').fadeOut(200,
-            function(){
-                status = 0;
-            }
-        );
-    })
-    //工具提示
-    $('[data-toggle="tooltip"]').tooltip();
-
-    ajax('POST','all.json','json');
-    var dataArr;
-
-    //页面加载完毕请求数据
+    //清空数据函数
+    function dataEmpty(obj){
+        obj.empty();
+    }
+    //封装的请求数据方法并根据数据创建内容
     function ajax(type,url,datatype){
         $.ajax({
             type:type,
@@ -59,8 +24,8 @@ $(function () {
                     "<a href='#'>"+value.title+"</a>"+
                     "<div class='bottom'>"+
                     "<div class='left'>"+
-                    "<span>"+value.person+"</span>"+
-                    "<span>"+value.leave+"</span>"+
+                    "<span>"+"<i class='fa fa-users'>"+"</i>"+value.person+"</span>"+
+                    "<span>"+"<i class='fa fa-commenting'>"+"</i>"+value.leave+"</span>"+
                     "</div>"+
                     "<div class='right'>"+
                     "<span>"+value.status+"</span>"+
@@ -72,7 +37,53 @@ $(function () {
         }
     }
 
-    //数据请求成功后遍历数据同时添加给对应的元素
+
+    //状态变量
+    var status = 0;
+
+
+    //头部导航特效
+    //鼠标放在第二个导航图标显示
+    $('.nav li').eq(1).hover(function () {
+        $('.class-name').show()
+    },function () {
+        $('.class-name').hide()
+    });
+
+
+//    搜索框放大
+    $('.search').focus(function () {
+        $(this).stop().animate({'width':'120px'},500).blur(function () {
+            $(this).stop().animate({'width':'70px'},500)
+        })
+    })
+
+
+//    二维码的显示和隐藏
+    $('#last .last_content .right .bottom>span').mouseenter(function(){
+        if(status===0){
+            status = 1;
+        }else{
+            return;
+        }
+        $(this).children('div').fadeIn(200);
+    }).mouseleave(function(){
+        $(this).children('div').fadeOut(200,
+            function(){
+                status = 0;
+            }
+        );
+    })
+    //工具提示
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //页面加载完毕请求数据
+    ajax('POST','all.json','json');
+    //数据存放的数组
+    var dataArr;
+
+
+    //一级分类点击筛选
     $('#tab .tab_content .list-group .oul:eq(0) li').click(function(){
         var def = $(this).attr('data-def');
         $(this).addClass('current').siblings().removeClass('current');
@@ -80,7 +91,7 @@ $(function () {
         ajax('POST',''+def+'.json','json');
     })
 
-
+    //二级分类点击筛选
     $('#tab .tab_content .list-group .oul:eq(1) li').click(function(){
         var def = $(this).attr('data-def');
         $(this).addClass('current').siblings().removeClass('current');
@@ -95,8 +106,8 @@ $(function () {
                     "<a href='#'>"+obj.title+"</a>"+
                     "<div class='bottom'>"+
                     "<div class='left'>"+
-                    "<span>"+obj.person+"</span>"+
-                    "<span>"+obj.leave+"</span>"+
+                    "<span>"+"<i class='fa fa-users'>"+"</i>"+obj.person+"</span>"+
+                    "<span>"+"<i class='fa fa-commenting'>"+"</i>"+obj.leave+"</span>"+
                     "</div>"+
                     "<div class='right'>"+
                     "<span>"+obj.status+"</span>"+
@@ -107,8 +118,20 @@ $(function () {
             }
         })
     })
-    //清空数据函数
-    function dataEmpty(obj){
-        obj.empty();
-    }
+
+    //滚动条监听
+    $('#backT').hide();
+    $(window).scroll(function(event){
+        //滚动距离大于150时显示返回顶部
+        if($(window).scrollTop()>150){
+            $('#backT').show();
+
+        }else{
+            $('#backT').hide();
+        }
+    })
+    //返回顶部点击事件
+    $('#backT').click(function(){
+        $('html,body').animate({scrollTop:0},600)
+    })
 });
